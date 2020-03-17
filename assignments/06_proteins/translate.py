9f251127d3ddc2cfe922f8d42c974058e9c3f336
Ken Youens-Clark <kyclark@gmail.com>
2020-03-17 15:47:30 -0700

    make required

--- assignments/06_proteins/translate.py
+++ assignments/06_proteins/translate.py
@@ -27,7 +27,7 @@ def get_args():
                         help='A file with codon translations',
                         metavar='FILE',
                         type=argparse.FileType('r'),
-                        default=None)
+                        required=True)
 
     #parser.add_argument('-o',
                   #      '--outfile',
